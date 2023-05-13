dc526dba153b90cfb79bf6ba72e38ecfabf110f4
aicha0001 <133348664+aicha0001@users.noreply.github.com>
2023-05-13 12:20:08 +0400

    rss-reader

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-
-
-
-let square = document.getElementById('square');
-let words = document.getElementById('words');
-
-document.onload= greeting();
-
-square.addEventListener('click', (event)=> clicked());
-// square.addEventListener('click', function clicked (event){}) 
-square.addEventListener('mouseover', (e)=> changeColour('gray'));
-square.addEventListener('mouseout', (e)=> changeColour('red'));
-
-function changeColour(colour){
-    square.style.background = colour;
-}
-
-function clicked(){
-    let msg = " Build a <br>" + createBuzzwordPhrase();
-    words.innerHTML = msg;
-
-}
-
-function greeting(){
-    words.innerHTML= "Welcome to Flatland. <br> I am Source";
-}
-   
-/*Buzzword Phrase Generator (JavaScript)*/
-
-function createBuzzwordPhrase() {
-    /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
-    let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
-    let action = ["empowered", "value-added", "synergy", "creative", "oriented", "focused", "aligned"];
-    let outcome = ["process", "deliverable", "solution", "tipping-point", "strategy", "vision"];
-
-    let idx_buz = Math.floor(Math.random() * buzz.length);
-    let idx_act = Math.floor(Math.random() * action.length);
-    let idx_out = Math.floor(Math.random() * outcome.length);
-
-    return  buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
-}
+function addRSStoDOM(data) {
+    let itemsContainer = document.createElement('DIV');
+  
+    for (let i = 0, t = data.items.length; i < t; ++i) {
+      let item = data.items[i]; 
+      let itemContainer = document.createElement('DIV');
+      let itemLinkElement = document.createElement('A');
+      itemLinkElement.setAttribute('href', item.link);
+      itemLinkElement.innerText = item.title;
+  
+      let itemTitleElement = document.createElement('H2');
+      itemTitleElement.appendChild(itemLinkElement);
+  
+      let itemDescriptionElement = document.createElement('P');
+      itemDescriptionElement.innerHTML = item.description;
+      itemContainer.appendChild(itemTitleElement);
+      itemContainer.appendChild(itemDescriptionElement);
+      itemsContainer.appendChild(itemContainer);
+    }
+    let titleElement = document.createElement('H1');
+    titleElement.innerText = data.feed.title;
+    content.appendChild(titleElement);
+    content.appendChild(itemsContainer);
+  }
+  var content = document.getElementsByTagName('main')[0]
+  
+  var xhr = new XMLHttpRequest(); 
+  xhr.onload = function(){
+    if(xhr.status >=200 && xhr.status < 300){
+      json = JSON.parse(xhr.responseText)
+      console.log(json)
+      addRSStoDOM(json) 
+    } 
+    else{
+      console.log("The request has failed")
+      content.innerHTML = "The request failed. Please check your RSS URL"
+    }
+  }
+  
+  let addFeedButton = document.getElementById("add-feed");
+  let newRSSInput = document.getElementById("rss-input");
+  
+  function onAddRSSClicked(event) {
+    let URL = newRSSInput.value;
+    newRSSInput.value = "";
+    xhr.open('GET', 'https://api.rss2json.com/v1/api.json?rss_url=' + URL);
+    xhr.send();
+  }
+  
+  addFeedButton.addEventListener('click', onAddRSSClicked);
+  
