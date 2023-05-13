c4023034a36931558b4612d54dda27f1df3a7b9e
aicha0001 <133348664+aicha0001@users.noreply.github.com>
2023-05-13 12:57:39 +0400

    flatland

--- script.js
+++ script.js
@@ -1,142 +1,41 @@
-// step 1 
-let addButton = document.getElementById('add-task')
-let newTaskInput = document.getElementById('task-input')
-let todoListContainer = document.getElementById('todolist')
 
-let templateElement = document.getElementById('list-item-template');
-let template = templateElement.innerHTML;
-let showActiveButton = document.getElementById("show-active")
-let showAllButton = document.getElementById("show-all")
-let showCompletedButton = document.getElementById("show-completed")
 
-// save to local storage 
-function saveTask(name, isCompleted){
-    localStorage.setItem(name,isCompleted)
+
+let square = document.getElementById('square');
+let words = document.getElementById('words');
+
+document.onload= greeting();
+
+square.addEventListener('click', (event)=> clicked());
+// square.addEventListener('click', function clicked (event){}) 
+square.addEventListener('mouseover', (e)=> changeColour('gray'));
+square.addEventListener('mouseout', (e)=> changeColour('red'));
+
+function changeColour(colour){
+    square.style.background = colour;
 }
 
-// function to read from local storage and read it 
-function renderTasks(){
-    for(let i=0; i<localStorage.length; i++){
-        let taskName = localStorage.key(i)
-        let isCompleted= localStorage.getItem(taskName) == "true";
-        
-        let taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
-
-        if(!isCompleted){
-            todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
-        }
-    }
+function clicked(){
+    let msg = " Build a <br>" + createBuzzwordPhrase();
+    words.innerHTML = msg;
 
 }
-// step 2 
-function onAddTaskClicked(e){
-    let taskName = newTaskInput.value;
-    newTaskInput.value= "";
 
-    if(taskName!=""){
-
-    let taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
-    todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
-    saveTask(taskName, false);
-    }
-    
+function greeting(){
+    words.innerHTML= "Welcome to Flatland. <br> I am Source";
 }
-
-// create the active task Button
-function onTodoListClicked(e){
-     let targetElement = e.target;
-    while (!targetElement.classList.contains("task")){
-        targetElement = targetElement.parentElement;
-    }
-    let checkbox = targetElement.querySelector(".checkbox");
-
-    if(checkbox.checked){
-        targetElement.classList.add("completed");
-    }
-    else{
-        targetElement.classList.remove("completed");
-    }
-    let TaskNameElement = targetElement.querySelector(".task-name");
-    let taskName = TaskNameElement.innerText;
-    
-    saveTask(taskName,checkbox.checked);
-}
-
-function showActiveTasks(e){
-    let tasks = document.getElementsByClassName("task");
-    for(let i=0; i<tasks.length; i++){
-        if (tasks[i].classList.contains("completed")){
-            tasks[i].style.display = "none";
-        }
-        else{
-            tasks[i].style.display= "block";
-        }
-    }
-
-}    
-// create the show all Task Button
-    function onTodoListClicked(e){
-        let targetElement = e.target;
-       while (!targetElement.classList.contains("task")){
-           targetElement = targetElement.parentElement;
-       }
-       let checkbox = targetElement.querySelector(".checkbox");
    
-       if(checkbox.checked){
-           targetElement.classList.add("completed");
-       }
-       else{
-           targetElement.classList.remove("completed");
-       }
-       let TaskNameElement = targetElement.querySelector(".task-name");
-       let taskName = TaskNameElement.innerText;
-       
-       saveTask(taskName,checkbox.checked);
-   }
-   
-   function showAllTasks(e){
-       let tasks = document.getElementsByClassName("task");
-       for(let i=0; i<tasks.length; i++){
-           tasks[i].style.display= "block";
-           }
-       }   
+/*Buzzword Phrase Generator (JavaScript)*/
 
-// create the completed button 
-function onTodoListClicked(e){
-    let targetElement = e.target;
-   while (!targetElement.classList.contains("task")){
-       targetElement = targetElement.parentElement;
-   }
-   let checkbox = targetElement.querySelector(".checkbox");
+function createBuzzwordPhrase() {
+    /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
+    let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
+    let action = ["empowered", "value-added", "synergy", "creative", "oriented", "focused", "aligned"];
+    let outcome = ["process", "deliverable", "solution", "tipping-point", "strategy", "vision"];
 
-   if(checkbox.checked){
-       targetElement.classList.add("completed");
-   }
-   else{
-       targetElement.classList.remove("active");
-   }
-   let TaskNameElement = targetElement.querySelector(".task-name");
-   let taskName = TaskNameElement.innerText;
-   
-   saveTask(taskName,checkbox.checked);
+    let idx_buz = Math.floor(Math.random() * buzz.length);
+    let idx_act = Math.floor(Math.random() * action.length);
+    let idx_out = Math.floor(Math.random() * outcome.length);
+
+    return  buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
 }
-
-function showCompletedTasks(e){
-   let tasks = document.getElementsByClassName("task");
-   for(let i=0; i<tasks.length; i++){
-       if (tasks[i].classList.contains("completed")){
-           tasks[i].style.display = "block";
-       }
-       else{
-           tasks[i].style.display= "none";
-       }
-   }
-}
-// step 3
-addButton.addEventListener('click', onAddTaskClicked)
-todoListContainer.addEventListener('click', onTodoListClicked);
-showActiveButton.addEventListener('click', showActiveTasks)
-showCompletedButton.addEventListener('click',showCompletedTasks)
-showAllButton.addEventListener('click',showAllTasks)
-renderTasks();
-
