ef57c29ee2945da3f5fe09b1ca151248312d7233
aicha0001 <133348664+aicha0001@users.noreply.github.com>
2023-05-13 12:24:03 +0400

    rock, paper , scissors

--- script.js
+++ script.js
@@ -1,51 +1,17 @@
-function addRSStoDOM(data) {
-    let itemsContainer = document.createElement('DIV');
-  
-    for (let i = 0, t = data.items.length; i < t; ++i) {
-      let item = data.items[i]; 
-      let itemContainer = document.createElement('DIV');
-      let itemLinkElement = document.createElement('A');
-      itemLinkElement.setAttribute('href', item.link);
-      itemLinkElement.innerText = item.title;
-  
-      let itemTitleElement = document.createElement('H2');
-      itemTitleElement.appendChild(itemLinkElement);
-  
-      let itemDescriptionElement = document.createElement('P');
-      itemDescriptionElement.innerHTML = item.description;
-      itemContainer.appendChild(itemTitleElement);
-      itemContainer.appendChild(itemDescriptionElement);
-      itemsContainer.appendChild(itemContainer);
+
+/* This is a simplified version of the game.
+The Computer ALWAYS picks ROCK.  
+Does the user's choose win,lose, or tie vs ROCK? */
+function play(user) {
+    let result = "";
+    if (user === 'rock') {
+        result = "tie";
     }
-    let titleElement = document.createElement('H1');
-    titleElement.innerText = data.feed.title;
-    content.appendChild(titleElement);
-    content.appendChild(itemsContainer);
-  }
-  var content = document.getElementsByTagName('main')[0]
-  
-  var xhr = new XMLHttpRequest(); 
-  xhr.onload = function(){
-    if(xhr.status >=200 && xhr.status < 300){
-      json = JSON.parse(xhr.responseText)
-      console.log(json)
-      addRSStoDOM(json) 
-    } 
-    else{
-      console.log("The request has failed")
-      content.innerHTML = "The request failed. Please check your RSS URL"
+    if (user === 'paper') {
+        result = "win";
     }
-  }
-  
-  let addFeedButton = document.getElementById("add-feed");
-  let newRSSInput = document.getElementById("rss-input");
-  
-  function onAddRSSClicked(event) {
-    let URL = newRSSInput.value;
-    newRSSInput.value = "";
-    xhr.open('GET', 'https://api.rss2json.com/v1/api.json?rss_url=' + URL);
-    xhr.send();
-  }
-  
-  addFeedButton.addEventListener('click', onAddRSSClicked);
-  
+    if (user === 'scissors') {
+        result = "lose";
+    }
+    alert("Computer chose rock, you " + result + "!");
+}
